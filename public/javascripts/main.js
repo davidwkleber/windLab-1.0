7c273836c828a0bb0368a8d619410de5e5b91a7e
davidwkleber <david.kleber@gmail.com>
2015-05-06 20:04:55 +0200

    navigation working
    JQuery style

---
+++ public/javascripts/main.js
@@ -0,0 +1,33 @@
+
+
+$(document).ready(function() {
+	
+	    $("#topFrameBody").click(function(event){
+			var target = event.target;
+			console.log("got into main "+target.id);
+			console.log(target.name);
+
+			switch(target.id) {
+				case "tourFrame":
+							console.log("got into tourFrame");	
+					window.parent.$("#dataFrame").empty();
+					window.parent.$("#dataFrame").load("/iFrameContent/index.html");
+					break;
+				case "powerCurveGraph":
+					console.log("got into powerGraph");
+					window.parent.$("#dataFrame").empty();
+					window.parent.$("#dataFrame").load("powerCurveGraph.html");
+					break;
+				case "timeDomainGraph":
+					console.log("got into timeGraph");
+					window.parent.$("#dataFrame").empty();
+					window.parent.$("#dataFrame").load("lineGraph.html");
+				break;
+				default :
+					console.log("id is: "+target.id);
+					break;
+			}
+	
+		})
+
+	})
