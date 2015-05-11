82a06b6004240fd41cf38ab712c228e963ce0854
davidwkleber <david.kleber@gmail.com>
2015-05-11 17:06:25 +0200

    frame action

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 					console.log("got into timeGraph");
 					window.parent.$("#dataFrame").empty();
 					window.parent.$("#dataFrame").load("lineGraph.html");
-					window.parent.$("#dataFrame").load("record.html");
+				//	window.parent.$("#dataFrame").load("record.html");
 
 				break;
 				default :
