2c9b5aa87b659a19a02eef7ec69f5c719a4147d4
davidwkleber <david.kleber@gmail.com>
2015-05-12 16:44:55 +0200

    Revert "Revert "graphs""
    This reverts commit 8cf25dd772140655943f44812703e99a02512585.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-dataFrameContent = 'timeDomainGraph';
+dataFrameContent = 'powerCurveGraph';
 iFrameContent = 'infoFrame';
 // var serialListener = require('./lib/serialListener');
 
